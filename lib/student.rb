6efa97f1e632d7c0db49d70fed06cf3095c448aa
Harry Bloch <harryfbloch@gmail.com>
2018-10-25 15:05:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,6 +34,6 @@ class Student
       VALUES (?,?)
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn].execute("SELECT id FROM students DESC LIMIT 1").strip
+      @id = DB[:conn].execute("SELECT id FROM students DESC LIMIT 1").flatten
   end
 end
