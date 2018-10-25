a87120cb6c7093dbc19e2743d2e06d80cdbab9c8
Harry Bloch <harryfbloch@gmail.com>
2018-10-25 14:48:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student
   attr_accessor :name, :grade
   #  with DB[:conn]  
   
-  def initialize(name, grade)
+  def initialize(name, grade, id = nil)
     self.name = name 
     self.grade = grade
   end
