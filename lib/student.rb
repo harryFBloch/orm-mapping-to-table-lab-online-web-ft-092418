f1130a2c94f2ef278bbccc9e25ca86e322199885
Harry Bloch <harryfbloch@gmail.com>
2018-10-25 15:04:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ class Student
       VALUES (?,?)
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
-      id = DB[:conn].execute("SELECT id FROM students DESC LIMIT 1")
+      self.id = DB[:conn].execute("SELECT id FROM students DESC LIMIT 1")
       binding.pry
   end
 end
